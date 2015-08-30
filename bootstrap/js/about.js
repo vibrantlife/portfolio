d0ce6913773935606cced23448b75472ac0abea1
vibrantlife <nicholesvibrantlife@gmail.com>
2015-08-29 17:55:52 -0700

    revamped about section

---
+++ bootstrap/js/about.js
@@ -0,0 +1,14 @@
+$('li').click(function(){
+    $('li').removeClass('active');
+    $(this).addClass('active');
+});
+
+$('nav li').on('click', function(event){
+    event.preventDefault();
+    var content = $(this).data();
+    var contentIds = ['#skills','#contact','#about'];
+    for( var contentName in contentIds ){
+        $(contentIds[contentName]).addClass('hidden');
+    }
+    $('#' + content.id).removeClass('hidden');
+})
